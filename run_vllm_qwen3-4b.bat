3b3c5d1272744a0e49e162748f2f574875741ebe
Kwangnam Yu <53140105+phykn@users.noreply.github.com>
2025-05-21 20:43:42 +0900

    Add files via upload

---
+++ run_vllm_qwen3-4b.bat
@@ -0,0 +1,12 @@
+@echo off
+
+wsl -d Ubuntu-24.04 -- bash -c ^
+"docker run --gpus all --ipc=host -p 8000:8000 ^
+-v /home/kn/models/Qwen3-4B-AWQ:/model ^
+vllm/vllm-openai:latest ^
+--model /model ^
+--max-model-len 8192 ^
+--enable-auto-tool-choice ^
+--tool-call-parser=hermes"
+
+cmd /k
